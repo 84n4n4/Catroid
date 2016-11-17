05b164c9015c0142f5331a531b20b4f96d870fc2
Thomas Hirsch <84n4n4@gmail.com>
2016-11-17 14:30:48 +0100

    ActivityTestRul introduced

---
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/SmokeTest.java
@@ -0,0 +1,63 @@
+package org.catrobat.catroid.uiespresso;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.catrobat.catroid.R;
+import org.catrobat.catroid.ui.MainMenuActivity;
+import org.catrobat.catroid.uiespresso.util.BaseActivityInstrumentationRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class SmokeTest{
+	private static final String TAG = SmokeTest.class.getSimpleName();
+
+	@Rule
+	public BaseActivityInstrumentationRule<MainMenuActivity> BaseActivityTestRule = new
+			BaseActivityInstrumentationRule<>(MainMenuActivity.class);
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+//
+//	@Test
+//	public void testYay(){
+//		onView(withId(R.id.main_menu_button_new))
+//				.perform(click());
+//		try {
+//			Thread.sleep(10000);
+//		}
+//		catch (InterruptedException e) {
+//			//donothing
+//		}
+//	}
+
+	@Test
+	public void newProject(){
+		try {
+			Thread.sleep(2000);
+		}
+		catch (InterruptedException e) {
+			//donothing
+		}
+		onView(withId(R.id.main_menu_button_new)).perform(click());
+		onView(withId(R.id.project_name_edittext)).perform(typeText("TestProject"));
+		onView(withText(R.string.ok)).perform(click());
+		onView(withText(R.string.ok)).perform(click());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+}
---
+++ catroid/src/androidTest/java/org/catrobat/catroid/uiespresso/util/BaseActivityInstrumentationRule.java
@@ -0,0 +1,64 @@
+package org.catrobat.catroid.uiespresso.util;
+
+import android.app.Activity;
+import android.os.Environment;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+
+import org.catrobat.catroid.common.Constants;
+import org.catrobat.catroid.stage.StageListener;
+import org.catrobat.catroid.test.utils.Reflection;
+
+import java.io.File;
+
+public class BaseActivityInstrumentationRule<T extends Activity> extends ActivityTestRule<T>
+{
+	private SystemAnimations systemAnimations;
+	private Class clazz;
+
+	public BaseActivityInstrumentationRule(Class<T> activityClass, boolean initialTouchMode, boolean launchActivity){
+		super(activityClass, initialTouchMode, launchActivity);
+		clazz = activityClass;
+	}
+
+	public BaseActivityInstrumentationRule(Class<T> activityClass, boolean initialTouchMode){
+		super(activityClass, initialTouchMode);
+		clazz = activityClass;
+	}
+
+	public BaseActivityInstrumentationRule(Class<T> activityClass){
+		super(activityClass);
+		clazz = activityClass;
+	}
+
+	@Override
+	protected void beforeActivityLaunched() {
+		super.beforeActivityLaunched();
+		Reflection.setPrivateField(StageListener.class, "checkIfAutomaticScreenshotShouldBeTaken", false);
+
+		Reflection.setPrivateField(Constants.class, "DEFAULT_ROOT", Environment.getExternalStorageDirectory().getAbsolutePath()
+				+ "/Pocket Code uiTest");
+		File uiTestFolder = new File(Constants.DEFAULT_ROOT);
+		if (uiTestFolder.exists()) {deleteRecursive(uiTestFolder);}
+	}
+
+	@Override
+	protected void afterActivityLaunched() {
+		systemAnimations = new SystemAnimations(InstrumentationRegistry.getTargetContext());
+		systemAnimations.disableAll();
+		super.afterActivityLaunched();
+	}
+
+	@Override
+	protected void afterActivityFinished() {
+		systemAnimations.enableAll();
+		super.afterActivityFinished();
+	}
+
+	void deleteRecursive(File fileOrDirectory) {
+		if (fileOrDirectory.isDirectory())
+			for (File child : fileOrDirectory.listFiles())
+				deleteRecursive(child);
+		fileOrDirectory.delete();
+	}
+}
